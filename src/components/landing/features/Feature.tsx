aba1564957b5959f8141919e1df0bfaec4e4c0b3
Weddly <weddly@hotmail.com>
2023-05-12 21:36:00 -0300

    Landing page features component

---
+++ src/components/landing/features/Feature.tsx
@@ -0,0 +1,38 @@
+import ResponsiveImage from "../shared/ResponsiveImage"
+
+export interface FeatureProps {
+    image: any
+    title: string
+    subtitle?: string
+    inverter?: boolean
+}
+
+export default function Feature (props: FeatureProps) {
+    return(
+        <div className={`
+            flex flex-col justify-around items-center w-full gap-6
+            ${props.inverter ? 'sm:flex-row-reverse' : 'sm:flex-row'}      
+        `}>
+            <ResponsiveImage 
+                image={props.image}
+                className={props.inverter ? 'sm:rotate-6' : 'sm:-rotate-6'}
+            />
+            <div className={`
+                flex flex-col gap-y-6 sm:w-[350px]
+                text-center sm:text-left
+            `}>
+                <div className={`
+                    flex flex-col text-white
+                    font-black text-2xl sm:text-4xl
+                `}>
+                    {props.title}
+                </div>
+                {props.subtitle && (
+                    <span className="font-light text-base sm:text-lg text-zinc-500">
+                        {props.subtitle}
+                    </span>
+                )}
+            </div>
+        </div>
+    )
+}
--- src/components/landing/features/index.tsx
+++ src/components/landing/features/index.tsx
@@ -1,7 +1,32 @@
+import Feature from "./Feature";
+import vantagem1 from "../../../../public/vantagem-1.jpg"
+import vantagem2 from "../../../../public/vantagem-2.jpg"
+import vantagem3 from "../../../../public/vantagem-3.jpg"
+import Area from "../shared/Area";
+
 export default function Features() {
     return(
-        <div>
+        <Area id="features" className="bg-black py-16 sm:py-36">
+            <div className="flex flex-col items-center gap-y-16 sm:gap-y-36">
+                <Feature 
+                    title="Complete and simple to use financial manager"
+                    subtitle="Here you can have complete control of your finances and an easy-to-understand visualization. The path of the economy begins here!" 
+                    image={vantagem1}
+                />
+                <Feature 
+                    title="Organized so you never forget an account again"
+                    subtitle="Visualize and track your finances day by day. The monthly organization will help you to have a clear control of income and expenses!" 
+                    image={vantagem2}
+                    inverter
+                />
+                <Feature 
+                    title="Ideal for financial planning"
+                    subtitle="Our No. 1 principle is to be a platform that makes financial control simple, so planning becomes a natural thing!"
+                    image={vantagem3}
+                />
+            </div>
             
-        </div>
+            
+        </Area>
     )
 }
